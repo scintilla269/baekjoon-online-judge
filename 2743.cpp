55b9e16303f4816bca155640c81d8bb43e511ecf
Dongyoung Mun <scintilla269@gmail.com>
2018-06-16 01:49:07 +0900

    add 2743.cpp

--- 2743.cpp
+++ 2743.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main() {
     string word;
-    cin >> word;
-    cout << word.size();
+    getline(cin, word);
+    for (auto const &x : word) {
+        if (isalpha(x)) {
+            if (('A' <= x && x <= 'M') || ('a' <= x && x <= 'm')) {
+                cout << char(x + 13);
+            } else {
+                cout << char(x - 13);
+            }
+        } else {
+            cout << x;
+        }
+    }
     return 0;
 }
