175441bb4377a96b961b9a3050e8114380a73547
Dongyoung Mun <scintilla269@gmail.com>
2018-06-17 17:06:10 +0900

    add 11022.cpp

---
+++ 11022.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, A, B;
+    cin >> T;
+    for (int i = 1; i <= T; ++i) {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A << " + " << B
+             << " = " << A + B << '\n';
+    }
+    return 0;
+}
