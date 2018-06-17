3c811393f8c751fdac46d2ff66629ff5f30f3fb3
Dongyoung Mun <scintilla269@gmail.com>
2018-06-17 19:07:50 +0900

    add 5522.cpp

---
+++ 5522.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 5; ++i) {
+        int score;
+        cin >> score;
+        sum += score;
+    }
+    cout << sum;
+    return 0;
+}
