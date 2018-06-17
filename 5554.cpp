f316947947c4d435079afb775d8f3c7a0d00ea94
Dongyoung Mun <scintilla269@gmail.com>
2018-06-17 19:05:47 +0900

    add 5554.cpp

---
+++ 5554.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 4; ++i) {
+        int second;
+        cin >> second;
+        sum += second;
+    }
+    cout << sum / 60 << '\n' << sum % 60;
+    return 0;
+}
