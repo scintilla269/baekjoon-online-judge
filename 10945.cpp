72980f013626a301c5c73290c618a46669180381
Dongyoung Mun <scintilla269@gmail.com>
2018-06-18 17:38:04 +0900

    add 10945.cpp

---
+++ 10945.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 10000);
+    cout << dis(gen);
+    return 0;
+}
