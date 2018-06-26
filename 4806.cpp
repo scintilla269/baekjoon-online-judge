8b2d93b16ec8dc2f405490b59981d3ba08ea158d
Dongyoung Mun <scintilla269@gmail.com>
2018-06-27 03:22:30 +0900

    add 4806.cpp

---
+++ 4806.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int cnt = 0;
+    string str;
+    while (getline(cin, str)) {
+        ++cnt;
+    }
+    cout << cnt;
+    return 0;
+}
