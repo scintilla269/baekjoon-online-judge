1d255accf8f10d73bb2a702898a0687387926c11
Dongyoung Mun <scintilla269@gmail.com>
2018-06-28 07:30:09 +0900

    add 2702.cpp

---
+++ 2702.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a%b);
+    }
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; ++i) {
+        int a, b;
+        cin >> a >> b;
+        cout << lcm(a, b) << ' ' << gcd(a, b) << '\n';
+    }
+    return 0;
+}
