8aeb8e55d975402c4ac3f4c7823b1ec560e109a8
Dongyoung Mun <scintilla269@gmail.com>
2018-06-28 13:33:30 +0900

    add 10599.cpp

---
+++ 10599.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if (a == 0 && b == 0 && c == 0 && d == 0) {
+            break;
+        }
+        cout << c - b << ' ' << d - a << '\n';
+    }
+    return 0;
+}
