e585d0be68bc8b871cc0fb3bf3e7d7f9da2a8cdf
Dongyoung Mun <scintilla269@gmail.com>
2018-07-07 21:16:56 +0900

    add 2164.cpp

---
+++ 2164.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    deque<int> deq(N);
+    for (int i = 0; i < N; ++i) {
+        deq[i] = i + 1;
+    }
+    while (deq.size() > 1) {
+        deq.pop_front();
+        deq.push_back(deq.front());
+        deq.pop_front();
+    }
+    cout << deq.front();
+    return 0;
+}
