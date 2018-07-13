e8caedeedf95aad998447663ee76da7e3f1469e5
Dongyoung Mun <scintilla269@gmail.com>
2018-07-13 20:41:17 +0900

    add 14581.cpp

---
+++ 14581.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string hongjun;
+    cin >> hongjun;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << hongjun << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+    return 0;
+}
