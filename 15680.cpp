0a06ab54efc97afba81d1140bc23a782017ceeaa
Dongyoung Mun <scintilla269@gmail.com>
2018-07-13 20:27:12 +0900

    add 15680.cpp

---
+++ 15680.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    switch (getc(stdin)) {
+        case '0':
+            printf("YONSEI");
+            break;
+        case '1':
+            printf("Leading the Way to the Future");
+            break;
+    }
+    return 0;
+}
