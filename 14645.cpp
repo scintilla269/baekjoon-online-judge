fe015447fa819cd7d90003f6fdfc051dbaa6b302
Dongyoung Mun <scintilla269@gmail.com>
2018-07-13 20:42:50 +0900

    add 14645.cpp

---
+++ 14645.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    for (int i = 0; i < N; ++i) {
+        int A, B;
+        cin >> A >> B;
+    }
+    cout << "비와이";
+    return 0;
+}
