f13b178658c1e55269775382d162cea84a54402a
Dongyoung Mun <scintilla269@gmail.com>
2018-07-13 20:49:53 +0900

    add 15857.cpp

---
+++ 15857.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    switch (getc(stdin)) {
+        case '1':
+            printf("a");
+            break;
+        case '2':
+            printf("b");
+            break;
+        case '3':
+            printf("b");
+            break;
+        case '4':
+            printf("c");
+            break;
+        case '5':
+            printf("d");
+            break;
+        case '6':
+            printf("d");
+            break;
+        case '7':
+            printf("d");
+            break;
+        case '8':
+            printf("c");
+            break;
+    }
+    return 0;
+}
