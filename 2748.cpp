d0e841c55d8827d4c2c90b08208f91880f5598c2
Dongyoung Mun <scintilla269@gmail.com>
2018-07-14 16:56:06 +0900

    add 2748.cpp

---
+++ 2748.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long prev = 0, curr = 0, next = 1;
+    for (int i = 0; i < n; ++i) {
+        prev = curr;
+        curr = next;
+        next = prev + curr;
+    }
+    cout << curr;
+    return 0;
+}
