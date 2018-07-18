75377359195e7f1c53d5e6269e8797fc0e881de5
Dongyoung Mun <scintilla269@gmail.com>
2018-07-19 04:36:50 +0900

    add 14909.cpp

---
+++ 14909.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, cnt = 0;
+    while (cin >> num) {
+        if (num > 0) {
+            ++cnt;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
