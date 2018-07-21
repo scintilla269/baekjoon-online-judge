d179625cba8f67126a00a024d3e7e5290b8b4edc
Dongyoung Mun <scintilla269@gmail.com>
2018-07-21 21:28:09 +0900

    add 11659.cpp

---
+++ 11659.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    vector<int> vec;
+    vec.reserve(N + 1);
+    vec.push_back(0);
+    for (int i = 0; i < N; ++i) {
+        int num;
+        cin >> num;
+        vec.push_back(num + vec.back());
+    }
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        cout << vec[b] - vec[a - 1] << '\n';
+    }
+    return 0;
+}
