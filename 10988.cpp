4e92cc2f995e24b57e3e74264141206d653fbd45
Dongyoung Mun <scintilla269@gmail.com>
2018-07-22 18:11:38 +0900

    add 10988.cpp

---
+++ 10988.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    for (int i = 0; i < word.size() / 2; ++i) {
+        if (word[i] != word[word.size() - 1 - i]) {
+            cout << 0;
+            return 0;
+        }
+    }
+    cout << 1;
+    return 0;
+}
