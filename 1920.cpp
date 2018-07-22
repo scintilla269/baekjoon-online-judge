3a1a4c9eb3381415b62231fb6664f584703a4abc
Dongyoung Mun <scintilla269@gmail.com>
2018-07-22 11:15:22 +0900

    add 1920.cpp

---
+++ 1920.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    unordered_set<int> S;
+    S.reserve(N);
+    for (int i = 0; i < N; ++i) {
+        int num;
+        cin >> num;
+        S.insert(num);
+    }
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; ++i) {
+        int num;
+        cin >> num;
+        cout << S.count(num) << '\n';
+    }
+    return 0;
+}
