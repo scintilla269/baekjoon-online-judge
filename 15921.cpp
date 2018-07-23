e409753b45421385f7ad6906b3a0338672e69764
Dongyoung Mun <scintilla269@gmail.com>
2018-07-23 11:16:56 +0900

    add 15921.cpp

---
+++ 15921.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        int num;
+        cin >> num;
+    }
+    cout << (N ? "1.00" : "divide by zero");
+    return 0;
+}
