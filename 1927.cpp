da15b0f5c840ea22db34598cc7031c1f8a1cc1d3
Dongyoung Mun <scintilla269@gmail.com>
2018-07-24 17:08:08 +0900

    add 1927.cpp

---
+++ 1927.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> vec;
+    for (int i = 0; i < N; ++i) {
+        int x;
+        cin >> x;
+        if (x) {
+            vec.push_back(x);
+            push_heap(vec.begin(), vec.end(), greater<int>());
+        } else {
+            if (vec.empty()) {
+                cout << 0;
+            } else {
+                pop_heap(vec.begin(), vec.end(), greater<int>());
+                cout << vec.back();
+                vec.pop_back();
+            }
+            cout << '\n';
+        }
+    }
+    return 0;
+}
