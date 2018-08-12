34d3b344dd9970659ebdb3c75436480946af0903
Dongyoung Mun <scintilla269@gmail.com>
2018-08-13 00:28:02 +0900

    add 1402.cpp

---
+++ 1402.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        cout << "yes\n";
+    }
+    return 0;
+}
