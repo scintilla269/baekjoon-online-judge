1174c88f92d220795a955985addd1f2c4bf2aae2
Dongyoung Mun <scintilla269@gmail.com>
2018-08-18 10:21:31 +0900

    add 15637.cpp

---
+++ 15637.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1:
+            cout << 103;
+            break;
+        case 2:
+            cout << 88;
+            break;
+        case 3:
+            cout << 91;
+            break;
+        case 4:
+            cout << 99;
+            break;
+        case 5:
+            cout << 101;
+            break;
+        case 6:
+            cout << 86;
+            break;
+        case 7:
+            cout << 96;
+            break;
+        case 8:
+            cout << 100;
+            break;
+        case 9:
+            cout << 71;
+            break;
+        case 10:
+            cout << 91;
+            break;
+        case 11:
+            cout << 96;
+            break;
+        case 12:
+            cout << 91;
+            break;
+        case 13:
+            cout << 98;
+            break;
+        case 14:
+            cout << 102;
+            break;
+        case 15:
+            cout << 96;
+            break;
+        case 16:
+            cout << 85;
+            break;
+        case 17:
+            cout << 102;
+            break;
+        case 18:
+            cout << 97;
+            break;
+        case 19:
+            cout << 91;
+            break;
+        case 20:
+            cout << 112;
+            break;
+        case 21:
+            cout << 85;
+            break;
+        case 22:
+            cout << 79;
+            break;
+        case 23:
+            cout << 86;
+            break;
+        case 24:
+            cout << 91;
+            break;
+        case 25:
+            cout << 97;
+            break;
+        case 26:
+            cout << 98;
+            break;
+        case 27:
+            cout << 110;
+            break;
+        case 28:
+            cout << 80;
+            break;
+        case 29:
+            cout << 88;
+            break;
+        case 30:
+            cout << 80;
+            break;
+        case 31:
+            cout << 95;
+            break;
+        case 32:
+            cout << 83;
+            break;
+        case 33:
+            cout << 93;
+            break;
+        case 34:
+            cout << 109;
+            break;
+        case 35:
+            cout << 88;
+            break;
+        case 36:
+            cout << 91;
+            break;
+        case 37:
+            cout << 103;
+            break;
+        case 38:
+            cout << 90;
+            break;
+        case 39:
+            cout << 95;
+            break;
+        case 40:
+            cout << 113;
+            break;
+        case 41:
+            cout << 81;
+            break;
+        case 42:
+            cout << 86;
+            break;
+        case 43:
+            cout << 97;
+            break;
+        case 44:
+            cout << 93;
+            break;
+        case 45:
+            cout << 93;
+            break;
+    }
+    return 0;
+}
