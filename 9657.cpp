ca889c9d666e5b7638442eb321c914090e10f924
Dongyoung Mun <scintilla269@gmail.com>
2018-08-22 22:15:00 +0900

    add 9657.cpp

---
+++ 9657.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    switch ((N-1) % 7) {
+        case 0:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            cout << "SK";
+            break;
+        case 1:
+        case 6:
+            cout << "CY";
+            break;
+    }
+    return 0;
+}
