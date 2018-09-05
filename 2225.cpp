453050ac00fb3686049c93f81c4aa2f028574d1e
Dongyoung Mun <scintilla269@gmail.com>
2018-09-05 13:54:25 +0900

    add 2225.cpp

---
+++ 2225.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<vector<int>> dp(K+1, vector<int>(N+1));
+    for (int i = 0; i <= N; ++i) {
+        dp[1][i] = 1;
+    }
+    for (int i = 2; i <= K; ++i) {
+        dp[i][0] = 1;
+        for (int j = 1; j <= N; ++j) {
+            dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000;
+        }
+    }
+    cout << dp[K][N];
+    return 0;
+}
