deeb9730cb015d4490e337b66121bcef8dbbe4f9
Dongyoung Mun <scintillavoy@gmail.com>
2018-12-15 01:50:46 +0900

    add 16204.cpp

---
+++ 16204.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, M, K;
+    cin >> N >> M >> K;
+    cout << min(M, K) + min (N - M, N - K) << '\n';
+    return 0;
+}
