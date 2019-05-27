68ff37ec2646ff2f3ede25f086891be933dc0e70
Dongyoung Moon <scintillavoy@gmail.com>
2019-05-27 12:17:03 +0900

    add 15891.cpp

---
+++ 15891.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    switch (N) {
+        case 1:
+            cout << 65;
+            break;
+        case 2:
+            cout << 17;
+            break;
+        case 3:
+            cout << 4;
+            break;
+        case 4:
+            cout << 4;
+            break;
+        case 5:
+            cout << 64;
+            break;
+    }
+    cout << '\n';
+    return 0;
+}
